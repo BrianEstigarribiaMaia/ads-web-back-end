d3d24d5b77acf6d4a044e09380e8fd838e8a6788
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-12 17:00:31 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+    <form action="mensagem.php" method="POST">
+        Nome: <input type="text" name="txtNome" >
+        Idade: <input type="text" name="txtIdade">
+        <button type="submit">Submeter</button>
+    </form>
+</body>
+</html>
---
+++ mensagem.php
@@ -0,0 +1,5 @@
+<?php
+    $nome = $_POST['txtNome'];
+    $idade = $_POST['txtIdade'];
+    echo "Nome: $nome idade: " . $idade;
+?>
