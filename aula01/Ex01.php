493b037c88796374f74d36c5126f30ce03e4f86c
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-13 16:24:35 -0300

    Add files via upload

---
+++ aula01/Ex01.php
@@ -0,0 +1,11 @@
+
+<?php 
+//5) Desenvolva um algoritmo que some os valores 2 e 4 sem solicitar os valores ao usuário.
+
+	$valor1 = 2;
+	$valor2 = 4;
+	
+	$soma = $valor1 + $valor2;
+	
+	echo "A soma de $valor1 + $valor2 = $soma";
+?>
