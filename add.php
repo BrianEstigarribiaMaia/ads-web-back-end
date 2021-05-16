4384c44afc5fab4d54bc1f87523531571690ab94
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-15 21:04:09 -0300

    Add files via upload

---
+++ add.php
@@ -0,0 +1,7 @@
+﻿<?php
+	//add - acrecentar periodo ao tempo
+	$data = new DateTime('2020-09-11');
+	print_r($data);
+	$data->add(new DateInterval('P2M5D')); //2 meses e 5 dias 
+	print_r($data);
+?>
