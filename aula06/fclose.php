984ab5db291f48dc64722ecbfd8c4aeddada6a7c
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-06-11 21:05:33 -0300

    Add files via upload

---
+++ aula06/fclose.php
@@ -0,0 +1,8 @@
+﻿<?php
+//FCLOSE - EXEMPLO 
+
+$handle = fopen("c:\\xampp\\htdocs\\sistemasWebAulas\\Aula06\\teste1.txt", "r"); //abre arquivo
+
+fclose($handle); //fecha arquivo
+
+?>
---
+++ aula06/feof.php
@@ -0,0 +1,11 @@
+﻿<?php
+//feof - verifica se chegou ao final do arquivo antes de fechar
+
+$file = fopen("c:\\xampp\\htdocs\\sistemasWebAulas\\Aula06\\teste1.txt","r");
+
+while(!feof($file)){
+	echo "Ainda não cheguei no final do arquivo";
+}
+
+fclose($file);
+?>
