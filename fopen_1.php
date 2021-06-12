503c721ca70fe96534186852438bc8e03cae6512
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-06-11 21:04:22 -0300

    Add files via upload

---
+++ fopen_1.php
@@ -0,0 +1,9 @@
+﻿<?php
+	//fopen abrir arquivos
+	
+	//abrindo url
+	$ponteiro = fopen("https://www.ftec.com.br","r");
+	
+	//abrindo arquivo
+	$ponteiro = fopen("C:\\xampp\\htdocs\\Aula06\\teste1.txt","r");
+?>
